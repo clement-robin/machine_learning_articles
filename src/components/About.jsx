5c191e82a525001b89bd87348848073d1ef25685
clement-robin <robin.clement78@gmail.com>
2023-06-09 15:33:50 -0400

    search button for back test

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+
+export default function AboutWrapper() {
+  return <About />; {}
+}
