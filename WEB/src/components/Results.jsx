7e3adb263637337fe8420988dd1b0f82cc9fa6ff
Marwane Bahraoui <bahraoui.marwane@gmail.com>
2023-06-16 15:11:28 -0400

    [back] fix web scrapping

--- WEB/src/components/Results.jsx
+++ WEB/src/components/Results.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 import { info } from '../assets';
-import { extract } from '@extractus/article-extractor';
+import { extract, extractFromHtml } from '@extractus/article-extractor';
 
 function Results({ title, text, link }) {
   const [data, setData] = useState([{}]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(0); // 0 = pas d'erreur / 1 = erreur de fetch d'article extractor
   const [hoveredModel, setHoveredModel] = useState(-1);
 
   
@@ -20,13 +21,24 @@ function Results({ title, text, link }) {
   useEffect(()=> {
     setLoading(true);
     const fetchData = async () => {
-      console.log(link);
+      //console.log('le lien : ',link);
+      var thetitle = "";
+      var thetext = "";
       if (link !== '') {
         try {
-          const article = await extract(link)
-          console.log(article)
+          const res = await fetch(link);
+          const html = await res.text();
+          const article = await extractFromHtml(html, link);
+          //console.log("texte de l'article : ", article.content);
+          //console.log("titre de l'article", article.title);
+          thetitle = article.title;
+          thetext = article.content;
+          if (article === null){
+            setError(1);
+          }
         } catch (err) {
-          console.error(err)
+          console.error(err);
+          return;
         }
       }
       try {
@@ -36,15 +48,15 @@ function Results({ title, text, link }) {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify({ title: title, text: text })
+          body: JSON.stringify({ title: thetitle, text: thetext })
         };
         const response = await fetch("http://localhost:5000/recherche", requestOptions);
         const responseData = await response.json();
         setData(responseData);
         setLoading(false);
-        console.log(responseData);
+        //console.log(responseData);
       } catch (error) {
-        console.error('Erreur lors de la prédiction du modèle', error);
+        console.error('Erreur lors de la recherche avec l\'api backend : ', error);
       }
     };
 
@@ -69,57 +81,71 @@ function Results({ title, text, link }) {
           <div className="border-t-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
         </div>
       ) : (
-        <div className="flex">
-          {data.map((modele, i) => (
-            <div
-              key={i}
-              className={`flex-1 ${i !== 0 ? 'border-l' : ''} text-center`}
-              onMouseEnter={() => setHoveredModel(i)}
-              onMouseLeave={() => setHoveredModel(-1)}
-            >
-              <div className="flex justify-center items-center">
-                <h1 className="text-xl">modèle {i}</h1>
-                <img
-                  src={info}
-                  alt="Info Logo"
-                  className="w-6 h-6 ml-5"
-                />
-              </div>
-              <div className="ml-8 mb-8 mt-6" style={{ visibility: hoveredModel === i ? 'visible' : 'hidden' }}>
-                <div className="flex justify-center items-center">
-                  <h2>R2</h2>
-                  <h3 className="font-bold text-lg ml-3">
-                    <animated.span>{animatedMetrics.r2.to(value => value.toFixed(6))}</animated.span>
-                  </h3>
-                  <h2 className="ml-4 mr-1">RMSE</h2>
-                  <h3 className="font-bold text-lg">
-                    <animated.span>{animatedMetrics.rmse.to(value => value.toFixed(0))}</animated.span>
-                  </h3>
-                </div>
-                <div className="flex justify-center items-center">
-                  <h2 className="mr-1">MAPE</h2>
-                  <h3 className="font-bold text-lg mr-4">
-                    <animated.span>{animatedMetrics.mape.to(value => value.toFixed(0))}</animated.span>
-                  </h3>
-                  <h2 className="mr-1">MAE</h2>
-                  <h3 className="font-bold text-lg">
-                    <animated.span>{animatedMetrics.mae.to(value => value.toFixed(0))}</animated.span>
-                  </h3>
-                </div>
-              </div>
-              <div>
-                {modele.fakeOrNot ? (
-                  <h1 className="text-7xl text-bold mt-2 mb-16" style={{ color: '#FF0000' }}>FAKE</h1>
-                ) : (
-                  <h1 className="text-7xl text-bold mt-2" style={{ color: '#00FF00' }}>TRUE</h1>)
-                }
-              </div>
-            </div>
-          ))}
-        </div>
+        <>
+          {(() => {
+            switch (error) {
+              case 0:
+                return (
+                  <div className="flex">
+                    {data.map((modele, i) => (
+                      <div
+                        key={i}
+                        className={`flex-1 ${i !== 0 ? 'border-l' : ''} text-center`}
+                        onMouseEnter={() => setHoveredModel(i)}
+                        onMouseLeave={() => setHoveredModel(-1)}
+                      >
+                        <div className="flex justify-center items-center">
+                          <h1 className="text-xl">modèle {i}</h1>
+                          <img
+                            src={info}
+                            alt="Info Logo"
+                            className="w-6 h-6 ml-5"
+                          />
+                        </div>
+                        <div className="ml-8 mb-8 mt-6" style={{ visibility: hoveredModel === i ? 'visible' : 'hidden' }}>
+                          <div className="flex justify-center items-center">
+                            <h2>R2</h2>
+                            <h3 className="font-bold text-lg ml-3">
+                              <animated.span>{animatedMetrics.r2.to(value => value.toFixed(6))}</animated.span>
+                            </h3>
+                            <h2 className="ml-4 mr-1">RMSE</h2>
+                            <h3 className="font-bold text-lg">
+                              <animated.span>{animatedMetrics.rmse.to(value => value.toFixed(0))}</animated.span>
+                            </h3>
+                          </div>
+                          <div className="flex justify-center items-center">
+                            <h2 className="mr-1">MAPE</h2>
+                            <h3 className="font-bold text-lg mr-4">
+                              <animated.span>{animatedMetrics.mape.to(value => value.toFixed(0))}</animated.span>
+                            </h3>
+                            <h2 className="mr-1">MAE</h2>
+                            <h3 className="font-bold text-lg">
+                              <animated.span>{animatedMetrics.mae.to(value => value.toFixed(0))}</animated.span>
+                            </h3>
+                          </div>
+                        </div>
+                        <div>
+                          {modele.fakeOrNot ? (
+                            <h1 className="text-7xl text-bold mt-2 mb-16" style={{ color: '#FF0000' }}>FAKE</h1>
+                          ) : (
+                            <h1 className="text-7xl text-bold mt-2" style={{ color: '#00FF00' }}>TRUE</h1>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                );
+              case 1:
+                return <p>Impossible d'atteindre la cible suivante : <a href={link}>{link}</a></p>;
+              default:
+                return <p>Erreur.</p>;
+            }
+          })()}
+          </>
       )}
     </div>
   );
+  
 }
 
 export default Results;
