5dece93d3805b64fdc207e021aa73e4dee8ec7c0
Marwane Bahraoui <bahraoui.marwane@gmail.com>
2023-06-18 18:01:16 -0400

    [back] remove useless functions

--- WEB/flask_server/server.py
+++ WEB/flask_server/server.py
@@ -1,12 +1,5 @@
-"""
-Projet -  
-
-authors :
-    Marwane Bahraoui - bahraoui.marwane@gmail.com
-"""
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from time import sleep
 import sys
 # setting path
 sys.path.append('../../../machine_learning_articles')
@@ -19,35 +12,16 @@ CORS(app)
 def statut():
     return "UP !"
 
-@app.route("/resultats")
-def resultats():
-    sleep(2)
-    return {"resultats": ["res1","res2","res3"]}
-
-@app.route("/recherche_titre/<titre>")
-def rechercheAvecTitre(titre):
-    #sleep(3)
-    predictions = mod.prediction_modele(titre,"aaa")
-    return "Vrai"+str(predictions)
-
 @app.route("/recherche", methods = ['POST'])
 def recherche():
     if request.method == 'POST':
         data = request.get_json()
         title, text = data['title'], data['text']
         if title=="" or text=="":
-            return "ERREUR - Un des arguments est vide"
+            return jsonify("ERREUR - Un des arguments est vide")
         predictions = mod.prediction_modele(title , text)
         return jsonify(predictions)
-    return "POST NE FONCTIONNENT PAS"
-
-@app.route("/testpost", methods = ['POST'])
-def testpost():
-    data = request.form
-    title, text = data.get('title'), data.get('text')
-    predictions = mod.prediction_modele(title , text)
-    return predictions
-
+    return jsonify("POST NE FONCTIONNENT PAS")
 
 if __name__ == '__main__':
     app.run(debug=True)
